5590fba4626dccdb300b3f0a738740464bf6c94b
NickNair07 <nitheeshkumar507@gmail.com>
2023-07-21 16:28:11 +0530

    added cart, pagination, search

---
+++ carts/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Cart, Cart_item
+from store.models import Product
+
+# Create your views here.
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)    # get the product with product id
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))   # get the cart using session key 
+
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(                 # if cart not exist, create cart using session key
+            cart_id = _cart_id(request)
+        )
+        cart.save()
+
+    try:
+        cart_item = Cart_item.objects.get(product=product, cart=cart)    # get cart item to the cart
+        cart_item.quantity += 1          # increase the cart item quantity
+        cart_item.save()
+    except Cart_item.DoesNotExist:          # adding the cart item first time with a session key-
+                                                                            # and setting the quantity = 1
+        cart_item = Cart_item.objects.create(
+            product=product,
+            quantity=1,
+            cart=cart,
+        )
+        cart_item.save()
+    return redirect('cart')
+
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id= _cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = Cart_item.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))      # get the cart using session key
+    product = get_object_or_404(Product, id=product_id)     
+    cart_item = Cart_item.objects.get(cart=cart, product=product)   # get the cart item related to the specific cart
+    cart_item.delete()
+    return redirect('cart')
+
+
+def cart(request, total=0, quantity=0, cart_items=None):
+    cart = Cart.objects.get(cart_id= _cart_id(request)) 
+    cart_items = Cart_item.objects.filter(cart=cart, is_active=True)
+
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+
+    tax = (2 * total)/100
+    grand_total = total + tax
+
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'quantity': quantity,
+        'tax': tax,
+        'grand_total': grand_total
+    }
+    return render(request, 'store/cart.html', context)
--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 
 
 # Create your views here.
@@ -9,12 +11,22 @@ def store(request, category_slug=None):     # None is set to avoid getting error
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+
+        # pagination for category vised products
+        paginator = Paginator(products, 2)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
+
+        # pagination for all products
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     context = {
-        'products': products,
+        'products': paged_products,
         'count': product_count
     }
     return render(request, 'store/store.html', context)
@@ -28,3 +40,16 @@ def product_detail(request, category_slug, product_slug):
         'single_product': single_product,
     }
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:                                                         # Q is used as alternative for (OR) in django
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products': products,
+        'count': product_count,
+    }
+    return render(request, 'store/store.html', context)
