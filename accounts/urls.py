d2f956b4358cc1cd8f066a53417e8dc14f9de810
NickNair07 <nitheeshkumar507@gmail.com>
2023-07-26 18:24:44 +0530

    forgot password using email

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,12 @@ urlpatterns = [
     path('login/',views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
+
+    # default url for dashboard (eg:http://127.0.0.1:9001/accounts/)
     path('', views.dashboard, name='dashboard'),
 
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('forgot-Paswword/', views.forgot_password, name='forgot_password'),
+    path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+    path('reset-password/', views.reset_password, name='reset_password'),
 ]
